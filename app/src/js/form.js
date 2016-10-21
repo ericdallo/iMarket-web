56f6acbf32c06cdb001d07945fbe0c15068c3f73
Eric Dallo <ericdallo06@hotmail.com>
2016-10-21 20:42:34 -0200

    Adiciona floating action button e validacao no cadastro

--- app/src/js/form.js
+++ app/src/js/form.js
@@ -1,5 +1,38 @@
-define('form', [], function() {
-    'use strict';
+define('form', ['doc'], function($) {
+    'use strict';    
+
+    var appendErrorClass = function(label) {
+        if (label.find('.invalid-field').isEmpty()) {
+            label.addClass('invalid-field');
+        }
+    };
+
+    var removeValidationErrors = function(form) {
+        form.find('.invalid-field').each(function(el) {
+            $(el).removeClass('invalid-field');
+        });
+    };
+
+    var isEmpty = function(value) {
+        if (value === null || value === undefined || value.trim() === '') {
+            return true;
+        }
+        return false;
+    };
+
+    var isValid = function(form) {
+        var valid = true;
+
+        removeValidationErrors(form);
+
+        form.find('[required]').each(function(element){
+            if (isEmpty($(element).val())) {
+                appendErrorClass($(element));
+                valid = false;
+            }
+        });
+        return valid;
+    };
 
     var applyMask = function(field, mask, event) {
         const BACKSPACE_KEY = 8;
@@ -41,12 +74,23 @@ define('form', [], function() {
         'isDigit': function(value) {
             return isDigit(value);
         },
-        'mask': function ($field, mask, maxValue) {
-            $field.on('keypress', function (event) {
+        'mask': function (field, mask, maxValue) {
+            $(field).on('keypress', function (event) {
                 if(this.value.length === maxValue || !isDigit(event.key)) {
                     event.preventDefault();
                 }
                 applyMask(this, mask, event);
+            });
+        },
+        'validate': function (form, configs) {
+            var $form = $(form);
+            $form.attr('novalidate', true);
+            $form.throttle('submit', function() {
+                if (isValid.call(this, $form)) {
+                    configs && configs.success && configs.success.apply(this, arguments);
+                } else {
+                    configs && configs.error && configs.error.apply(this, arguments);
+                }
             });
         }
     }
--- app/src/js/shop-register.js
+++ app/src/js/shop-register.js
@@ -5,13 +5,27 @@ define(['doc', 'form'], function($, form) {
         var isChecked = this.checked;
 
         $('.shop-register .required').toggleClass('disabled');
+        $('.shop-register .fab').toggleClass('fab-disabled');
 
         $('.mandatory').each(function(element) {
             element.disabled = !isChecked;
         });
     });
 
-    form.mask($('.shop-register .cep'), "#####-###", form.CEP_SIZE);
-    form.mask($('.shop-register .cnpj'), "##.###.###/####-##", form.CNPJ_SIZE);
+    form.mask('.shop-register .cep', "#####-###", form.CEP_SIZE);
+    form.mask('.shop-register .cnpj', "##.###.###/####-##", form.CNPJ_SIZE);
+
+    form.validate('.shop-register', {
+        success: function() {
+            
+        },
+        error: function() {
+            var $invalidMessage = $('.shop-register .invalid-message');
+            $invalidMessage.toggleClass('hide');
+            setTimeout(function() {
+                $invalidMessage.addClass('hide');
+            } ,4000);
+        }
+    });
 
 });
