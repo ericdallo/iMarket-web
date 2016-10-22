41afde4dbddf3aeecb03f6e3b9740c33739482e0
Eric Dallo <ericdallo06@hotmail.com>
2016-10-22 16:22:17 -0200

    Corrige home

--- app/src/js/home.js
+++ app/src/js/home.js
@@ -1,5 +1,5 @@
 define(['doc', 'form'], function($, form) {
 	'use strict'
 
-	form.mask($('.cep-form .field'), "#####-###", form.CEP_SIZE);
+	form.mask('.cep-form .field', "#####-###", form.CEP_SIZE);
 });
