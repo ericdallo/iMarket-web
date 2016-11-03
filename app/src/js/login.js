620535e1fc4aae3549dd5dd07906373d0594fd15
Eric Dallo <ericdallo06@hotmail.com>
2016-11-02 22:03:30 -0200

    Adiciona login pelo facebook

--- app/src/js/login.js
+++ app/src/js/login.js
@@ -1,9 +1,9 @@
 define(['doc', 'ajax', 'form', 'loggedUser', 'ENV'], function($, ajax, form, $loggedUser, ENV) {
-	'use strict'
+    'use strict'
 
-	var $form = $('#loginForm');
+    var $form = $('#loginForm');
 
-	form.validate('#loginForm', {
+    form.validate('#loginForm', {
         success: function(event) {
             event.preventDefault;
 
@@ -14,7 +14,7 @@ define(['doc', 'ajax', 'form', 'loggedUser', 'ENV'], function($, ajax, form, $lo
 
             ajax.post(ENV.api.login, loginData, {
                 'success': function(response, xhr) {
-                	$loggedUser.store(response);                	
+                    $loggedUser.store(response);                    
                     window.location = "/";
                 },
                 'error': function(response, xhr) {
@@ -27,6 +27,20 @@ define(['doc', 'ajax', 'form', 'loggedUser', 'ENV'], function($, ajax, form, $lo
         error: function() {
             showMessage('.empty-fields-message');
         }
+    });
+
+    $form.find('.btn-facebook').on('click', function(event) {
+        event.preventDefault();
+        FB.login(function(response){
+          if (response.status === 'connected') {
+            window.location = '/';
+            console.log('con',response);
+          } else if (response.status === 'not_authorized') {
+            console.log('not_a',response);
+          } else {
+            console.log('erro');
+          }
+        }, {scope: 'public_profile,email'});
     });
 
     var showMessage = function(messageClass) {
