e68847cd7800b604176a2548583efcfc193cb4c2
Eric Dallo <ericdallo06@hotmail.com>
2016-11-03 00:39:21 -0200

    Deploy

--- production/js/env.js
+++ production/js/env.js
@@ -1,1 +1,1 @@
-define("ENV",[],function(){"use strict";var t="http://localhost:9090/";return{api:{premarkets:t+"premarkets",login:t+"login"}}});
+define("ENV",[],function(){"use strict";var e="http://localhost:9090/";return{api:{premarkets:e+"premarkets",login:e+"login",register:e+"register"}}});
