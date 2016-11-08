a3c6c37bda489ca70f83e1d32b3a3477cafeb0f1
Eric Dallo <ericdallo06@hotmail.com>
2016-11-08 18:23:38 -0200

    Corrige deploy

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -25,21 +25,6 @@ systemctl enable docker
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 
-### DEPLOY
-APP=imarket-web
-BUCKET_DIR=/tmp/bucket
-
-mkdir -p $BUCKET_DIR
-gcsfuse configuration.imarketbr.com $BUCKET_DIR
-
-cp $BUCKET_DIR/$APP/prod/production.js /tmp
-fusermount -u $BUCKET_DIR
-rm -r $BUCKET_DIR
-
-docker pull imarket/$APP:latest
-if docker ps | awk -v app="$APP" 'NR>1{  ($(NF) == $APP )  }'; then
-  docker stop "$APP" && docker rm -f "$APP"
-fi
-docker run --name $APP -d -p 80:8080 -v /tmp/production.js:/opt/app/app/src/js/env.js imarket/$APP
 echo "$DEPLOY_SCRIPT" > /opt/deploy.sh
 chmod +x /opt/deploy.sh
+bash /opt/deploy.sh
