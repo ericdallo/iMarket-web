b202c37bba9563e2ac79415090357a15bcca6c5a
Eric Dallo <ericdallo06@hotmail.com>
2016-11-08 18:19:14 -0200

    Adiicona noindex ao home

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-set -ve
 INSTANCE_NAME=$(gcloud compute instance-groups list-instances imarket-web-group --zone us-east1-d |grep imarket-web-group | awk '{print $1}')
 gcloud compute ssh $INSTANCE_NAME --zone us-east1-d --command /opt/deploy.sh
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -37,7 +37,7 @@ fusermount -u $BUCKET_DIR
 rm -r $BUCKET_DIR
 
 docker pull imarket/$APP:latest
-if docker ps | awk -v app="APP" 'NR>1{  ($(NF) == APP )  }'; then
+if docker ps | awk -v app="$APP" 'NR>1{  ($(NF) == $APP )  }'; then
   docker stop "$APP" && docker rm -f "$APP"
 fi
 docker run --name $APP -d -p 80:8080 -v /tmp/production.js:/opt/app/app/src/js/env.js imarket/$APP
