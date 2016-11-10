94ac5a4518ac12ead85585e5c7496d589307ad56
Eric Dallo <ericdallo06@hotmail.com>
2016-11-10 10:00:15 -0200

    Update deploy-in-instance.sh

--- configuration/deploy-in-instance.sh
+++ configuration/deploy-in-instance.sh
@@ -3,6 +3,8 @@
 APP=imarket-web
 
 docker pull imarket/$APP:latest
-if docker ps | awk -v app="APP" 'NR>1{  ($(NF) == APP )  }'; then docker stop "$APP" && docker rm -f "$APP" fi
+if docker ps | awk -v app="APP" 'NR>1{  ($(NF) == APP )  }'; then 
+    docker stop "$APP" && docker rm -f "$APP" 
+fi
 	
 docker run --name $APP -d -p 8080:8080 -v /opt/production.js:/opt/app/app/src/js/env.js imarket/$APP
