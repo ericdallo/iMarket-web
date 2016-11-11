4f9323540b3e42cb4fae2cb2200ac6e6791390f7
Eric Dallo <ericdallo06@hotmail.com>
2016-11-10 22:10:15 -0200

    Corrige identacao do script

--- configuration/deploy-in-instance.sh
+++ configuration/deploy-in-instance.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-APP=imarket-web
+APP_WEB=imarket-web
 
-docker pull imarket/$APP:latest
-if docker ps | awk -v app="APP" 'NR>1{  ($(NF) == APP )  }'; then docker stop "$APP" && docker rm -f "$APP" fi
-	
-docker run --name $APP -d -p 8080:8080 -v /opt/production.js:/opt/app/app/src/js/env.js imarket/$APP
+docker pull imarket/$APP_WEB:latest
+if docker ps | awk -v app="APP_WEB" 'NR>1{  ($(NF) == APP_WEB )  }'; then 
+	docker stop "$APP_WEB" && docker rm -f "$APP_WEB" 
+fi
+docker run --name $APP_WEB -d -p 8080:8080 -v /opt/production.js:/opt/app/app/src/js/env.js imarket/$APP_WEB
