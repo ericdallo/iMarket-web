bceaecddede8f6fa4082354f8328ea0c7b67ab77
Eric Dallo <ericdallo06@hotmail.com>
2016-11-12 14:24:38 -0200

    Corrige startup

--- configuration/deploy-in-instance.sh
+++ configuration/deploy-in-instance.sh
@@ -6,10 +6,10 @@ IMARKET_WEB_PROPERTIES=/opt/production.js
 cp -rfv /opt/bucket/$APP_WEB/prod/production.js $IMARKET_WEB_PROPERTIES
 
 if docker ps | awk -v app="APP_WEB" 'NR>1{  ($(NF) == APP_WEB )  }'; then 
-	docker stop "$APP_WEB" && docker rm -f "$APP_WEB" 
+    docker stop "$APP_WEB" && docker rm -f "$APP_WEB" 
 fi
-docker run --name $APP_WEB -d -p 8080:8080 \
-	-v /opt/production.js:/opt/production.js \
-	-v /opt/imarketbr.com.crt:/opt/app/imarketbr.com.crt \
-	-v /opt/imarketbr.com.key:/opt/app/imarketbr.com.key \
-	imarket/$APP_WEB
+docker run --name $APP_WEB -it --entrypoint=/bin/bash -p 8080:8080 \
+    -v /opt/production.js:/opt/production.js \
+    -v /opt/imarketbr.com.crt:/opt/imarketbr.com.crt \
+    -v /opt/imarketbr.com.key:/opt/imarketbr.com.key \
+    imarket/$APP_WEB
--- docker/startup.sh
+++ docker/startup.sh
@@ -2,10 +2,11 @@
 set -ve
 
 git clone https://github.com/iMarketbr/iMarket-web.git $IMARKET_WEB_HOME
-cd $IMARKET_WEB_HOME
 
-IMARKET_WEB_PROPERTIES=/opt/production.js
-cp $IMARKET_WEB_PROPERTIES $IMARKET_WEB_HOME/app/src/js/env.js
+cd $IMARKET_WEB_HOME
+cp /opt/production.js $IMARKET_WEB_HOME/app/src/js/env.js
+cp /opt/imarketbr.com.crt $IMARKET_WEB_HOME
+cp /opt/imarketbr.com.key $IMARKET_WEB_HOME
 
 ./node_modules/bower/bin/bower install --allow-root
 exec npm run deploy
