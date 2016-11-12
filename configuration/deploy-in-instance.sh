51830865fa4dc16cee0d364c06acf802445869cf
Eric Dallo <ericdallo06@hotmail.com>
2016-11-12 14:41:28 -0200

    Corrige deploy

--- configuration/deploy-in-instance.sh
+++ configuration/deploy-in-instance.sh
@@ -8,7 +8,7 @@ cp -rfv /opt/bucket/$APP_WEB/prod/production.js $IMARKET_WEB_PROPERTIES
 if docker ps | awk -v app="APP_WEB" 'NR>1{  ($(NF) == APP_WEB )  }'; then 
     docker stop "$APP_WEB" && docker rm -f "$APP_WEB" 
 fi
-docker run --name $APP_WEB -it --entrypoint=/bin/bash -p 8080:8080 \
+docker run --name $APP_WEB -d -p 8080:8080 \
     -v /opt/production.js:/opt/production.js \
     -v /opt/imarketbr.com.crt:/opt/imarketbr.com.crt \
     -v /opt/imarketbr.com.key:/opt/imarketbr.com.key \
