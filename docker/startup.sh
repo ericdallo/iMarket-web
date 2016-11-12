6a2682a447b1de2c1b800a6b4b61feb3cb42ddb1
Eric Dallo <ericdallo06@hotmail.com>
2016-11-12 14:29:42 -0200

    Adicioan node_modules

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,9 +4,12 @@ set -ve
 git clone https://github.com/iMarketbr/iMarket-web.git $IMARKET_WEB_HOME
 
 cd $IMARKET_WEB_HOME
+
 cp /opt/production.js $IMARKET_WEB_HOME/app/src/js/env.js
 cp /opt/imarketbr.com.crt $IMARKET_WEB_HOME
 cp /opt/imarketbr.com.key $IMARKET_WEB_HOME
+cp -rf /opt/app/node_modules $IMARKET_WEB_HOME
 
 ./node_modules/bower/bin/bower install --allow-root
 exec npm run deploy
+
