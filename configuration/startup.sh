fac6fc8f8e75bd38b961792f5af6f77f1d5387ee
Eric Dallo <ericdallo06@hotmail.com>
2016-11-17 21:22:21 -0200

    Corrige deploy

--- configuration/startup.sh
+++ configuration/startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ve
 
+APP=imarket-web
+
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
 
-apt-get install apt-transport-https ca-certificates
-
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-export DOCKER_USER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/docker_user -H "Metadata-Flavor: Google")
-export DOCKER_PASS=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/docker_pass -H "Metadata-Flavor: Google")
-export DEPLOY_SCRIPT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/deploy_script -H "Metadata-Flavor: Google")
+GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+DOCKER_USER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/docker_user -H "Metadata-Flavor: Google")
+DOCKER_PASS=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/docker_pass -H "Metadata-Flavor: Google")
+DEPLOY_SCRIPT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/deploy_script -H "Metadata-Flavor: Google")
 
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -23,6 +23,7 @@ systemctl start docker
 systemctl enable docker
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
+docker pull imarket/$APP
 
 # Mount bucket
 BUCKET_DIR=/opt/bucket
@@ -30,7 +31,7 @@ BUCKET_DIR=/opt/bucket
 mkdir -p $BUCKET_DIR
 gcsfuse configuration.imarketbr.com $BUCKET_DIR
 
-cp -rfv $BUCKET_DIR/imarket-web/prod/nginx.conf /opt
+cp -rfv $BUCKET_DIR/$APP/prod/nginx.conf /opt
 cp -rfv $BUCKET_DIR/ssl/imarketbr.com.crt /opt
 cp -rfv $BUCKET_DIR/ssl/imarketbr.com.key /opt
 
@@ -40,14 +41,14 @@ chmod +x /opt/deploy.sh
 bash /opt/deploy.sh
 
 # Nginx container
-APP=nginx
-docker pull $APP
-if docker ps | awk -v app="APP" 'NR>1{  ($(NF) == APP )  }'; then 
-    docker stop "$APP" && docker rm -f "$APP" 
+NGINX=nginx
+docker pull $NGINX
+if docker ps | awk -v app="NGINX" 'NR>1{  ($(NF) == NGINX )  }'; then 
+    docker stop "$NGINX" && docker rm -f "$NGINX" 
 fi
 
 docker run --name $APP -d \
     -v /opt/nginx.conf:/etc/nginx/nginx.conf \
     -v /opt/imarketbr.com.crt:/etc/nginx/ssl/imarketbr.com.crt \
     -v /opt/imarketbr.com.key:/etc/nginx/ssl/imarketbr.com.key \
-    --link imarket-web:imarket-web -p 80:80 -p 443:443 $APP
+    --link $APP:$APP -p 80:80 -p 443:443 $APP
