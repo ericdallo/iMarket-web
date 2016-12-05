fe21c978c3045f771dfab3ae47fa9b0f02c27d3d
Eric Dallo and Gabriel Lima Gomes <login+eric.dallo+gabriel.gomes@elo7.com>
2016-12-05 13:37:52 -0200

    Adiciona manifest.json

--- app/context.js
+++ app/context.js
@@ -4,7 +4,8 @@ var vars = {};
 
 vars.cssDir = '/css/'
 vars.jsDir = '/js/'
-vars.imagesDir = '/assets/images/';
+vars.assetsDir = '/assets/';
+vars.imagesDir = vars.assetsDir + 'images/';
 
 vars.states = ['AC','AL','AM','AP','BA','CE','DF','ES','GO','MA','MG','MS','MT','PA','PB','PE','PI','PR','RJ','RN','RO','RR','RS','SC','SP','SE','TO'].toString();
 module.exports = { vars };
